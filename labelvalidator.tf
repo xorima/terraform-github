338f8f3d23e98ef49cf8fdbf1fbdba8b8499fd1e
Jason Field <jason@avon-lea.co.uk>
2020-10-24 21:09:03 +0100

    configs

--- labelvalidator.tf
+++ labelvalidator.tf
@@ -3,8 +3,8 @@ module "labelvalidator" {
   name                       = "labelvalidator"
   description                = "Ruby webapp to check that github prs have release labels"
   dockerhub_config           = local.dockerhub_config
-  label_validator_config     = label_validator_config
-  json_version_bumper_config = json_version_bumper_config
+  label_validator_config     = local.label_validator_config
+  json_version_bumper_config = local.json_version_bumper_config
   # release_creator_config = release_creator_config
   providers = {
     github              = github
